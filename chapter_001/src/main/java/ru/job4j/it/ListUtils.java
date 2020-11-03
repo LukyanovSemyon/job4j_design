7361ff7d5b986a11e528dedba3b7bfe59d083b4f
LukyanovSemyon <lukyakha@gmail.com>
2020-11-03 06:41:26 +0300

    7. ListIterator[350217#387675]

---
+++ chapter_001/src/main/java/ru/job4j/it/ListUtils.java
@@ -0,0 +1,67 @@
+package ru.job4j.it;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index) {
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index + 1) {
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> List<T> removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.remove();
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.remove();
+                i.add(value);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> j = elements.listIterator();
+        while (j.hasNext()) {
+            T el = j.next();
+            ListIterator<T> i = list.listIterator();
+            while (i.hasNext()) {
+                if (i.next().equals(el)) {
+                    i.remove();
+                }
+            }
+        }
+        return list;
+    }
+
+}
