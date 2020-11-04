4cf34b1d675ab56f4f955126bb34054fd5fe739f
LukyanovSemyon <lukyakha@gmail.com>
2020-11-05 00:53:33 +0300

    2. Без переопределения equals и hashCode[1005#387708]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -12,4 +12,17 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 2, new GregorianCalendar(1990, Calendar.FEBRUARY, 5));
+        User user2 = new User("Ivan", 2, new GregorianCalendar(1990, Calendar.FEBRUARY, 5));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, user2.getName());
+        map.put(user2, user2.getName());
+        System.out.println(map);
+    }
 }
