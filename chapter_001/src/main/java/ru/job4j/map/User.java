48c0cdf2fd74e857770128db55c9f13d141c12b4
LukyanovSemyon <lukyakha@gmail.com>
2020-11-06 01:38:51 +0300

    5. Переопределить и equals и hashCode[1002#387705] Correction#2

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -3,9 +3,9 @@ package ru.job4j.map;
 import java.util.*;
 
 public class User {
-    final String name;
-    final int children;
-    final Calendar birthday;
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -19,7 +19,7 @@ public class User {
 
     public static void main(String[] args) {
         User user1 = new User("Ivan", 2, new GregorianCalendar(1990, Calendar.FEBRUARY, 5));
-        User user2 = new User("Ivan", 2, new GregorianCalendar(1990, Calendar.FEBRUARY, 5));
+        User user2 = new User("Ivan", 1, new GregorianCalendar(1990, Calendar.FEBRUARY, 5));
         Map<User, Object> map = new HashMap<>();
         map.put(user1, user1.getName());
         map.put(user2, user2.getName());
