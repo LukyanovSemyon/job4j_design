1ae8b7b19bcaefdc7d9e4964bdcfa9594c95ccd2
LukyanovSemyon <lukyakha@gmail.com>
2020-11-11 13:08:32 +0300

    1. Читаем файл конфигурации[858#387730] Correction#1

--- chapter_002/src/main/java/ru/job4j/io/Config.java
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -3,22 +3,21 @@ package ru.job4j.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Config {
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private Map<String, String> values = new HashMap<String, String>();
 
     public Config(final String path) {
         this.path = path;
     }
 
     public void load() {
-        List<String> lines = new ArrayList<>();
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            read.lines().forEach(lines::add);
-            for (String line : lines) {
-                    values.put(line.split("=")[0], line.split("=")[1]);
-            }
+            values = read.lines().filter(line -> !line.isEmpty())
+                    .filter(line -> !line.contains("//"))
+                    .collect(Collectors.toMap(line -> line.split("=")[0], line -> line.split("=")[1]));
         } catch (Exception e) {
             e.printStackTrace();
         }
