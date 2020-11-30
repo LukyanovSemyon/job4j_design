244c7d398bf4f04f135f7e5411d619082f060c5d
LukyanovSemyon <lukyakha@gmail.com>
2020-11-30 19:26:46 +0300

    3.0. Тестирование IO[173905#387722] Correction#1

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -18,8 +18,9 @@ public class AnalizyTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01\n" + "500 10:57:01\n" + "400 10:58:01\n"
-                    + "200 10:59:01\n" + "500 11:01:02\n" + "200 11:02:02");
+            out.println("200 10:56:01" + System.lineSeparator() + "500 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator() + "200 10:59:01" + System.lineSeparator()
+                    + "500 11:01:02" + System.lineSeparator() + "200 11:02:02");
         }
         Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
 
@@ -27,6 +28,6 @@ public class AnalizyTest {
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(l -> rsl.append(l).append(System.lineSeparator()));
         }
-        assertThat(rsl.toString(), is("10:57:01;10:59:01\r\n11:01:02;11:02:02\r\n"));
+        assertThat(rsl.toString(), is("10:57:01;10:59:01" + System.lineSeparator() + "11:01:02;11:02:02" + System.lineSeparator()));
     }
 }
