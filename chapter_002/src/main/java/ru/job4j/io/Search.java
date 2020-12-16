37e26962cc22c193ced0d6895d5a1e3bd5b0cb9b
LukyanovSemyon <lukyakha@gmail.com>
2020-12-16 15:44:46 +0300

    5. Валидация параметров запуска.[246865#387723] Correction#1

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -8,15 +8,13 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar");
+        if (args.length <= 1) {
+            throw new IllegalArgumentException(((args.length == 0) ? "Root folder is null." : "Sought file extension is null.")
+                    + " Usage java -jar search.jar");
         }
-        if (args.length == 1) {
-            throw new IllegalArgumentException("Sought file extension is null. Usage java -jar search.jar");
-        }
-        Path start = Paths.get(args[0]);
-        String str = args[1];
-        search(start, str).forEach(System.out::println);
+            Path start = Paths.get(args[0]);
+            String str = args[1];
+            search(start, str).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
