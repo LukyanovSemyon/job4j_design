26e65aaf0bdd382cfc3552256411f89a1052bc94
LukyanovSemyon <lukyakha@gmail.com>
2020-12-18 13:25:48 +0300

    6. Кодировка.[862#387733]

---
+++ chapter_002/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,74 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ConsoleChat {
+    private final String path;
+    private final String botAnswers;
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private static List<String> answers = new ArrayList<>();
+    private static List<String> log = new ArrayList<>();
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
+            answers = in.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log("Консольный чат, напишите сообщение");
+        Scanner ing = new Scanner(System.in);
+        String userMessage = "";
+        while (!userMessage.equals(OUT)) {
+            userMessage = ing.nextLine();
+            log.add(userMessage);
+            log(answerBot());
+            switch (userMessage) {
+                case CONTINUE -> System.out.println(answerBot());
+                case STOP -> {
+                    log("Чат остановлен, для продолжения напишите: продолжить");
+                    while (!userMessage.equals(CONTINUE)) {
+                        userMessage = ing.nextLine();
+                        log.add(userMessage);
+                    }
+                    log(answerBot());
+                }
+                case OUT -> log("Чат прекращен");
+            }
+        }
+        try (BufferedWriter out = new BufferedWriter(
+                new FileWriter(path, Charset.forName("WINDOWS-1251"), true)
+            )) {
+            for (String line : log) {
+                out.write(line + System.lineSeparator());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void log(String str) {
+        System.out.println(str);
+        log.add(str);
+    }
+
+    public String answerBot() {
+        return answers.get((int) (Math.random() * answers.size()));
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("./chapter_002/data/ConsoleChatLog.txt", "./chapter_002/data/BotAnswers.txt");
+        cc.run();
+    }
+}
